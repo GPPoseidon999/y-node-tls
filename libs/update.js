915278b564eefb3ab92fce0b7882c160a23927af
俞鹏飞 <yupengfei@aifin.ltd>
2020-07-08 19:04:29 +0800

    update code

--- libs/update.js
+++ libs/update.js
@@ -1,15 +1,24 @@
 const axios = require('axios')
-const compareVersion = require('compare-versions')
+const color = require('cli-color')
+const terminalLink = require('terminal-link')
+const compareVersions = require('compare-versions')
 
 module.exports = async (v) => {
     const { data } = await axios.get('https://nodejs.org/dist/index.json')
+
     return data
         .filter((node) => {
-            const cp = v ? compareVersion(node.version, 'v' + v + '.0.0') >= 0 : true
+            const cp = v ? compareVersions(node.version, 'v' + v + '.0.0') >= 0 : true
             return node.lts && cp
         })
         .map((item) => {
             const { files, ...rest } = item
-            return { ...rest }
+            const doc = color.yellow(
+                terminalLink(
+                    'API',
+                    `https://nodejs.org/dist/${item.version}/docs/api/documentation.html`
+                )
+            )
+            return { ...rest, doc }
         })
 }
